64f576dedddc231755012cc969ccb3d5bd1fb904
jaeyoung.choi <lactrious@gmail.com>
2020-11-15 23:22:12 +0900

    Support mobile layout

--- pages/src/pages/FirstPage.tsx
+++ pages/src/pages/FirstPage.tsx
@@ -2,13 +2,14 @@ import * as React from "react";
 
 import ArticleListPage from "./ArticleListPage";
 import { Link } from "react-router-dom";
+import isLogged from "../apis/credential/isLogged";
 import metadata from "../metadata.json";
 
 export default function FirstPage() {
   return (
     <div className="Blog">
       <div className="Admin">
-        <Link to="/login">Login</Link>
+        <Link to="/login">{isLogged() ? "Logout" : "Login"}</Link>
       </div>
       <h1 className="BlogTitle">{metadata.title}</h1>
       <ArticleListPage />
