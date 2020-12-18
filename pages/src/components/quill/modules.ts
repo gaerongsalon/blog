39e985f66e6bd047638fcf1ccf7722559e91d01f
jaeyoung.choi <lactrious@gmail.com>
2020-12-18 22:08:37 +0900

    (pages) Bugfix on layout

--- pages/src/components/quill/modules.ts
+++ pages/src/components/quill/modules.ts
@@ -8,7 +8,7 @@ const modules = {
       // [{ script: "sub" }, { script: "super" }], // superscript/subscript
       // [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
 
-      // [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+      [{ size: ["small", false, "large", "huge"] }], // custom dropdown
       [{ header: [1, 2, 3, /*4, 5, 6,*/ false] }],
 
       [{ color: [] }, { background: [] }], // dropdown with defaults from theme
