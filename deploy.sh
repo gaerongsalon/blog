fd38b92ba9e622baeb666ff6032aa4422e12ed10
jaeyoung.choi <lactrious@gmail.com>
2020-12-20 13:32:18 +0900

    (ops) Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+TARGET="${1:-"pages"}"
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+if [ "${TARGET}" = "pages" ]; then
+  echo "Deploy pages"
+  pushd "${SCRIPT_PATH}/pages"
+    yarn && yarn build
+  popd
+  pushd "${SCRIPT_PATH}/api"
+  yarn && yarn deploy -f serveHtml
+  popd
+
+elif [ "${TARGET}" = "api" ]; then
+  echo "Deploy api"
+  pushd "${SCRIPT_PATH}/api"
+  yarn && yarn deploy
+  popd
+
+else
+  echo "Deploy all"
+  pushd "${SCRIPT_PATH}/pages"
+    yarn && yarn build
+  popd
+  pushd "${SCRIPT_PATH}/api"
+  yarn && yarn deploy
+  popd
+
+fi
+
