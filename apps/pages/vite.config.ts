90d80fb2877a773d834722d705698f3451af65db
jaeyoung.choi <lactrious@gmail.com>
2023-10-24 19:26:20 +0900

    (pages) Add adsbygoogle.js

--- apps/pages/vite.config.ts
+++ apps/pages/vite.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), VitePluginHtmlEnv()],
+  plugins: [
+    react(),
+    VitePluginHtmlEnv({
+      compiler: true,
+    }),
+  ],
   server: {
     proxy: {
       "/api": "http://localhost:3000",
